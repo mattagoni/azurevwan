df572434ef670bf50a750b39420f2be40e34738a
mattagoni <46663698+mattagoni@users.noreply.github.com>
2024-04-09 16:26:05 +1000

    Update createvms.ps1

--- createvms.ps1
+++ createvms.ps1
@@ -7,15 +7,3 @@ Remove-AzureRmPublicIpAddress -Name coles-vic-vm01-ip -ResourceGroupName magoni_
 az network nic ip-config update --name ipconfig1 --resource-group magoni_vms-rg --nic-name vic-vm01910 --remove PublicIpAddress
 Remove-AzureRmPublicIpAddress -Name coles-vic-vm02-ip -ResourceGroupName magoni_vms-rg -force
 
-
-##Set dns name to colesjumphost.australiasoutheast.cloudapp.azure.com
-# Get the network interface object
-$nic = Get-AzNetworkInterface -Name "vic-vm01911" -ResourceGroupName "magoni_vms-rg"
-# Get the public IP address object associated with the network interface
-$publicIp = Get-AzPublicIpAddress -Name $nic.IpConfigurations[0].PublicIpAddress.Id.Split('/')[-1] -ResourceGroupName $nic.ResourceGroupName
-# Set the DNS label for the public IP address
-$publicIp.DnsSettings = @{
-    "DomainNameLabel" = "colesjumphost"
-}
-# Update the public IP address
-Set-AzPublicIpAddress -PublicIpAddress $publicIp
