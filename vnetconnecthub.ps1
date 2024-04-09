f6efde2d1673d0e4cd39a3324b51d6ff268120f0
mattagoni <46663698+mattagoni@users.noreply.github.com>
2024-04-09 15:55:27 +1000

    Update vnetconnecthub.ps1

--- vnetconnecthub.ps1
+++ vnetconnecthub.ps1
@@ -2,7 +2,7 @@ $resourceGroup = Get-AzResourceGroup -ResourceGroupName "magoni_vwan-rg"
 $virtualWan = Get-AzVirtualWan -ResourceGroupName "magoni_vwan-rg" -Name "magoni_vwan"
 $virtualHub = Get-AzVirtualHub -ResourceGroupName "magoni_vwan-rg" -Name "Hub1"
 $remoteVirtualNetwork = Get-AzVirtualNetwork -Name "VNet1" -ResourceGroupName "magoni-vnet-rg"
-New-AzVirtualHubVnetConnection -ResourceGroupName "magoni-vnet-rg" -VirtualHubName "Hub1" -Name "vnet1-to-hub" -RemoteVirtualNetwork $remoteVirtualNetwork
+New-AzVirtualHubVnetConnection -ResourceGroupName "magoni_vwan-rg" -VirtualHubName "Hub1" -Name "vnet1-to-hub" -RemoteVirtualNetwork $remoteVirtualNetwork
 
 $resourceGroup = Get-AzResourceGroup -ResourceGroupName "magoni_vwan-rg" 
 $virtualWan = Get-AzVirtualWan -ResourceGroupName "magoni_vwan-rg" -Name "magoni_vwan"
