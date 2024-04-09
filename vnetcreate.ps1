fb70db61a04227563b8f0b9a1fd8aa26f41a407e
mattagoni <46663698+mattagoni@users.noreply.github.com>
2024-04-09 15:27:41 +1000

    Update vnetcreate.ps1

--- vnetcreate.ps1
+++ vnetcreate.ps1
@@ -4,13 +4,13 @@
 New-AzureRmResourceGroup -Name magoni-vnet-rg -Location AustraliaSouthEast
 
 ##Create vnet1 for vwan testing
-$virtualNetwork = New-AzVirtualNetwork -ResourceGroupName magoni-VicNetwork-rg -Location AustraliaSouthEast -Name vNET1 -AddressPrefix 10.1.0.0/16
+$virtualNetwork = New-AzVirtualNetwork -ResourceGroupName magoni-vnet-rg -Location AustraliaSouthEast -Name vNET1 -AddressPrefix 10.1.0.0/16
 $subnetConfig = Add-AzVirtualNetworkSubnetConfig -Name network1 -AddressPrefix 192.168.1.0/24 -VirtualNetwork $virtualNetwork
 $subnetConfig = Add-AzVirtualNetworkSubnetConfig -Name network2 -AddressPrefix 192.168.2.0/24 -VirtualNetwork $virtualNetwork
 $virtualNetwork | Set-AzVirtualNetwork
 
 ##Create vnet2 for vwan testing
-$virtualNetwork = New-AzVirtualNetwork -ResourceGroupName vNET2-rg -Location AustraliaSouthEast -Name vNET2 -AddressPrefix 172.16.0.0/16
+$virtualNetwork = New-AzVirtualNetwork -ResourceGroupName magoni-vnet-rg -Location AustraliaSouthEast -Name vNET2 -AddressPrefix 172.16.0.0/16
 $subnetConfig = Add-AzVirtualNetworkSubnetConfig -Name network1 -AddressPrefix 172.16.1.0/24 -VirtualNetwork $virtualNetwork
 $subnetConfig = Add-AzVirtualNetworkSubnetConfig -Name network2 -AddressPrefix 172.16.2.0/24 -VirtualNetwork $virtualNetwork
 $virtualNetwork | Set-AzVirtualNetwork
