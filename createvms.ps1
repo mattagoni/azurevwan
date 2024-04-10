dfa1608af7ed78bb667f0e4db04e5de75672d0c2
mattagoni <46663698+mattagoni@users.noreply.github.com>
2024-04-10 10:37:36 +1000

    Update createvms.ps1

--- createvms.ps1
+++ createvms.ps1
@@ -5,5 +5,5 @@ New-AzResourceGroupDeployment -ResourceGroupName "magoni_vms-rg" -TemplateFile "
 az network nic ip-config update --name ipconfig1 --resource-group magoni_vms-rg --nic-name magoni-vm01909 --remove PublicIpAddress
 Remove-AzureRmPublicIpAddress -Name magoni-vm01-ip -ResourceGroupName magoni_vms-rg -force
 az network nic ip-config update --name ipconfig1 --resource-group magoni_vms-rg --nic-name vic-vm01910 --remove PublicIpAddress
-Remove-AzureRmPublicIpAddress -Name coles-vic-vm02-ip -ResourceGroupName magoni_vms-rg -force
+Remove-AzureRmPublicIpAddress -Name magoni-vm02-ip -ResourceGroupName magoni_vms-rg -force
 
